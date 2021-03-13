7c33d73b2124e64f8ed6b2899a25095a55da7d55
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-03-13 13:26:24 +0100

    changes docker name

--- src/Backend/PictureStore/PictureStore.Web.Api/Docker/start.sh
+++ src/Backend/PictureStore/PictureStore.Web.Api/Docker/start.sh
@@ -1,13 +1,13 @@
 # Pulls then deletes current web api container
-docker pull eragonwien/picturestorewebapi:latest
-docker stop picturestorewebapi-container
-docker rm picturestorewebapi-container
+docker pull eragonwien/picturestore.web.api:latest
+docker stop picturestore.web.api-container
+docker rm picturestore.web.api-container
 
 # Runs new web api container
-docker run --name=picturestorewebapi-container 
+docker run --name=picturestore.web.api-container 
 	--restart=always 
 	-p 5000:80 
 	-v /home/pi/picture-store/upload_files:/upload_files 
 	-v /home/pi/picture-store/download_files:/download_files 
 	-d 
-	eragonwien/picturestorewebapi
+	eragonwien/picturestore.web.api
