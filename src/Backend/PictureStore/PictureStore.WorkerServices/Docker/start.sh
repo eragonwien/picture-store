a9a643f9a0236a79777247528ee5686926969db9
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-03-14 08:00:52 +0100

    updates start script

--- src/Backend/PictureStore/PictureStore.WorkerServices/Docker/start.sh
+++ src/Backend/PictureStore/PictureStore.WorkerServices/Docker/start.sh
@@ -6,8 +6,7 @@ docker rm picturestore.workerservices-container
 # Runs new web api container
 docker run --name=picturestore.workerservices-container 
 	--restart=always 
-	-p 5000:80 
 	-v /home/pi/picture-store/upload_files:/upload_files 
 	-v /home/pi/picture-store/download_files:/download_files 
 	-d 
-	eragonwien/picturestorewebapi
+	eragonwien/picturestore.workerservices
