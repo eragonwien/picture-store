02a0c76f0faffd0178ca5f3783d09e71b8b65201
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-03-21 14:26:43 +0100

    moves error handling in one place

--- src/Backend/PictureStore/PictureStore.Web.Api/Models/ErrorResponseModel.cs
+++ src/Backend/PictureStore/PictureStore.Web.Api/Models/ErrorResponseModel.cs
@@ -1,44 +1,21 @@
-﻿using PictureStore.Core.Exceptions;
+﻿using Newtonsoft.Json;
+using PictureStore.Core.Exceptions;
 using System;
 using System.IO;
-using System.Linq;
 
 namespace PictureStore.Web.Api.Models
 {
    public class ErrorResponseModel
    {
-      public string Type { get; set; }
       public string Message { get; set; }
+
+      [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
       public string StackTrace { get; set; }
 
-      public ErrorResponseModel(Exception exception, bool isDevelopment)
+      public ErrorResponseModel(string message, string stackTrace)
       {
-         Type = exception.GetType().Name;
-         Message = exception.Message;
-         StackTrace = isDevelopment ? exception.StackTrace : null;
-
-         HandleException(exception);
-      }
-
-      private void HandleException(Exception exception)
-      {
-         switch (exception)
-         {
-            case FileNotFoundException fileNotFoundException:
-               Message = "File not found";
-               break;
-            case UploadFileEmptyException uploadFileEmptyException:
-               Message = "Input file is required";
-               break;
-            case UploadStorageNotFoundException uploadStorageUnreachableException:
-               Message = "Storage cannot be reached. Please make sure that the hard drive is plugged in";
-               break;
-            case MoveToDownloadFolderException moveToDownloadFolderException:
-               Message = $"Error transfering file to download folder: {moveToDownloadFolderException.Message}";
-               break;
-            default:
-               break;
-         }
+         Message = message;
+         StackTrace = stackTrace;
       }
    }
 }
--- src/Backend/PictureStore/PictureStore.Web.Api/Startup.cs
+++ src/Backend/PictureStore/PictureStore.Web.Api/Startup.cs
@@ -76,23 +76,40 @@ namespace PictureStore.Web.Api
             if (errorContext is null) return;
 
             var statusCode = HttpStatusCode.InternalServerError;
+            string message = null; ;
+            string stackTrace = null;
 
             switch (errorContext.Error)
             {
                case UploadFileEmptyException uploadFileEmptyException:
                   statusCode = HttpStatusCode.BadRequest;
+                  message = "Input file is required";
+                  break;
+               case UploadStorageNotFoundException uploadStorageUnreachableException:
+                  statusCode = HttpStatusCode.BadRequest;
+                  message = "Storage cannot be reached. Please make sure that the hard drive is plugged in";
                   break;
                case FileNotFoundException fileNotFoundException:
                   statusCode = HttpStatusCode.NotFound;
+                  message = "File not found";
+                  break;
+               case MoveToDownloadFolderException moveToDownloadFolderException:
+                  statusCode = HttpStatusCode.InternalServerError;
+                  message = $"Error transfering file to download folder: {moveToDownloadFolderException.Message}";
                   break;
                default:
+                  statusCode = HttpStatusCode.InternalServerError;
+                  message = $"Please contact admin";
                   break;
             }
 
             context.Response.StatusCode = (int)statusCode;
             context.Response.ContentType = "application/json";
 
-            var result = new ErrorResponseModel(errorContext.Error, env.IsDevelopment());
+            if (!env.IsDevelopment()) 
+               stackTrace = null;
+
+            var result = new ErrorResponseModel(message, stackTrace);
             await context.Response.WriteAsJsonAsync(result);
          });
       }
