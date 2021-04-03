ec2288ef052d050091a5b964a60744a7f09bdb6b
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-04-04 00:33:52 +0200

    updates scripts

--- src/Backend/PictureStore/Scripts/crontab.sh
+++ src/Backend/PictureStore/Scripts/crontab.sh
@@ -2,6 +2,7 @@
 0 4 * * * /sbin/shutdown -r +5
 
 # Pull new docker images and start docker container after reboot
-@reboot /home/pi/picture-store/setup/start.sh
+0 4 * * * /shared/picturestore/setup/pull.sh +10
 
-
+# Pull new docker images and start docker container after reboot
+0 4 * * * service nginx restart +15
--- src/Backend/PictureStore/Scripts/pull.sh
+++ src/Backend/PictureStore/Scripts/pull.sh
@@ -1,10 +1,19 @@
+
+# Sets environment variables
+export PICTURE_STORE_HOME=/picturestore
+export PICTURE_STORE_MEDIA=/media/mybook/picturestore
+
+# Prepares directories
+mkdir $PICTURE_STORE_MEDIA/uploads
+mkdir $PICTURE_STORE_MEDIA/downloads
+
 # Pulls then deletes current web api container
 docker pull eragonwien/picturestore.web.api:latest
 docker stop picturestore.web.api-container
 docker rm picturestore.web.api-container
 
 # Runs new web api container
-docker run --name=picturestore.web.api-container --restart=always -p 5000:80 -v /home/pi/picture-store/upload_files:/upload_files -v /home/pi/picture-store/download_files:/download_files -d eragonwien/picturestore.web.api
+docker run --name=picturestore.web.api-container --restart=always -p 5000:80 -v $PICTURE_STORE_MEDIA/uploads:/upload_files -v $PICTURE_STORE_MEDIA/downloads:/download_files -d eragonwien/picturestore.web.api
     
 # Pulls then deletes current workerservices container
 docker pull eragonwien/picturestore.workerservices:latest
@@ -12,4 +21,7 @@ docker stop picturestore.workerservices-container
 docker rm picturestore.workerservices-container
 
 # Runs new workerservices container
-docker run --name=picturestore.workerservices-container --restart=always -v /home/pi/picture-store/upload_files:/upload_files -v /home/pi/picture-store/download_files:/download_files -d eragonwien/picturestore.workerservices
+docker run --name=picturestore.workerservices-container --restart=always -v $PICTURE_STORE_MEDIA/uploads:/upload_files -v $PICTURE_STORE_MEDIA/downloads:/download_files -d eragonwien/picturestore.workerservices
+
+# Restarts nginx
+service nginx restart
