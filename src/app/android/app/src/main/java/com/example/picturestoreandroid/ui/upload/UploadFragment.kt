182205226a1a345e311e22a0c6316bc1f9eb43de
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-05-13 08:01:37 +0200

    adds empty android project

---
+++ src/app/android/app/src/main/java/com/example/picturestoreandroid/ui/upload/UploadFragment.kt
@@ -0,0 +1,24 @@
+package com.example.picturestoreandroid.ui.upload
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.picturestoreandroid.R
+
+class UploadFragment : Fragment() {
+
+    private lateinit var uploadViewModel: UploadViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
