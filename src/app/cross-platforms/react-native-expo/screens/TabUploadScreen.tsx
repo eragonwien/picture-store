51ef9ac9bb53c07c7ec6761cb9a7e63dcfff3a7e
Hoang Son Nguyen <nguyennhoangson@gmail.com>
2021-06-07 16:17:20 +0200

    set expected layout of upload page

--- src/app/cross-platforms/react-native-expo/screens/TabUploadScreen.tsx
+++ src/app/cross-platforms/react-native-expo/screens/TabUploadScreen.tsx
@@ -1,40 +1,15 @@
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons';
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import { IconButton } from "../components/IconButton";
+import { Image, StyleSheet } from "react-native";
+import { Badge, LinearProgress, ListItem, useTheme } from "react-native-elements";
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
+import { ScrollView } from 'react-native-gesture-handler';
 import { ImageUploadView } from "../components/ImageUploadView";
-import { View, Text, largeSize, TitleText } from "../components/Themed";
+import { View, largeSize, TitleText } from "../components/Themed";
 
 export const TabUploadScreen = ({ }: {}) => {
-  const renderItem = ({ item }: { item: any }) => {
-    return (
-      <View style={styles.listItem}>
-        <View
-          style={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <View>
-            <Text>wird hochgeladen</Text>
-            <Text>68% - 12 Minuten verbleiben</Text>
-          </View>
-          <View style={{ display: "flex", flexDirection: "row" }}>
-            <IconButton
-              icon={<MaterialIcons name="speaker" size={32} color="black" />}
-            />
-            <IconButton
-              icon={<MaterialIcons name="video-call" size={32} color="black" />}
-            />
-          </View>
-        </View>
-      </View>
-    );
-  };
+
+  const theme = useTheme();
 
   return (
     <View style={styles.container}>
@@ -43,11 +18,20 @@ export const TabUploadScreen = ({ }: {}) => {
         <TitleText style={{ padding: 8, textAlign: "left", width: "100%" }}>
           Hochgeladene Bilder
         </TitleText>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
+        <ScrollView style={{ width: "100%" }}>
+          {
+            DATA.map((e, i) => (
+              <ListItem key={i} bottomDivider>
+                <Avatar source={{ uri: e.uri }} imageProps={{ resizeMode: "contain" }} />
+                <ListItem.Content>
+                  <ListItem.Title>{e.title}</ListItem.Title>
+                  <LinearProgress color="primary" value={0.5} variant="determinate" style={{ marginTop: 8 }} />
+                </ListItem.Content>
+                <MaterialIcons name="check-circle" color="green" size={32} />
+              </ListItem>
+            ))
+          }
+        </ScrollView>
       </View>
     </View>
   );
@@ -69,24 +53,34 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     margin: 8,
   },
+  flatList: {
+    width: "100%",
+  },
   listTitle: {
     fontSize: largeSize,
     fontWeight: "bold",
   },
   listItem: {
-    maxWidth: "100%",
-    height: 64,
+    width: "100%",
+    backgroundColor: 'green',
+    marginBottom: 8,
     display: "flex",
     flexDirection: "row",
+    justifyContent: "flex-start",
     alignItems: "center",
-    justifyContent: "space-between",
-    borderWidth: 4,
-    borderRadius: 16,
-    shadowColor: "black",
-    shadowOpacity: 50,
-    padding: 8,
-    marginBottom: 8,
   },
+  headingImage: {
+    width: 64,
+    height: 64,
+    margin: 4,
+    backgroundColor: 'red'
+  },
+  listItemStatusBarContainer: {
+    width: "100%"
+  },
+  listITemStatusBar: {
+    width: "100%"
+  }
 });
 
 const DATA = [
@@ -94,60 +88,72 @@ const DATA = [
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba12",
     title: "First Item",
     progress: "100%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f6311",
     title: "Second Item",
     progress: "50%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d7210",
     title: "Third Item",
     progress: "10%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba9",
     title: "First Item",
     progress: "100%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f638",
     title: "Second Item",
     progress: "50%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d727",
     title: "Third Item",
     progress: "10%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba6",
     title: "First Item",
     progress: "100%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f635",
     title: "Second Item",
     progress: "50%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d724",
     title: "Third Item",
     progress: "10%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba3",
     title: "First Item",
     progress: "100%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f632",
     title: "Second Item",
     progress: "50%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d721",
     title: "Third Item",
     progress: "10%",
+    uri: "https://bulma.io/images/bulma-logo.png",
   },
 ];
